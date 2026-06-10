d90631635ab382bc1d278d940fce87a6ff8830f0
respaldoMichelle2022-ui <respaldosmichelle.com>
2026-06-10 11:48:19 -0600

    Run seeders automatically in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,5 +12,8 @@ echo "Ejecutando migraciones pendientes..."
 # La bandera --force es necesaria porque estamos en producción (APP_ENV=production)
 php artisan migrate --force
 
+echo "Ejecutando seeders..."
+php artisan db:seed --force
+
 echo "Iniciando Nginx y PHP-FPM a través de Supervisor..."
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
